95cf62902a348a80857595a9a7a64073be6a6b45
tempzero <16360308@qq.com>
2018-07-05 10:20:34 +0800

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#Python 27
+import psutil
+import sqlite3
+import time
+
+db_name = 'mydb.db'
+
+def create_db():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    c.execute('''DROP TABLE IF EXISTS cpu''')  
+    c.execute(
+        '''CREATE TABLE cpu (id INTEGER PRIMARY KEY AUTOINCREMENT, insert_time text,cpu1 float, cpu2 float, cpu3 float, cpu4 float)''')
+    conn.close()
+
+
+def save_to_db(data):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('INSERT INTO cpu(insert_time,cpu1,cpu2,cpu3,cpu4) VALUES (?,?,?,?,?)', data)
+    conn.commit()
+    conn.close()
+
+create_db()
+
+while True:
+    cpus = psutil.cpu_percent(interval=1, percpu=True)
+    t = time.strftime('%M:%S', time.localtime())
+    save_to_db((t, cpus[0], cpus[1], cpus[2], cpus[3]))
+    print('save a data')
